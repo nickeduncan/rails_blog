59de8eebd8daf52814abe56dee983f6d5d0b4af3
Nick Duncan <nickeduncan@gmail.com>
2017-02-18 23:18:42 -0500

    project complete

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,11 @@
+class ApplicationController < ActionController::Base
+
+  http_basic_authenticate_with name: "dhh", password: "secret", except: [:index,
+    :show]
+
+    def index
+      @articles = Article.all
+    end
+    
+  protect_from_forgery with: :exception
+end
